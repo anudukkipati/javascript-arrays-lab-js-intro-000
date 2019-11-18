cf29dab97c20cd3dc78237e6291085d18efed4ce
Anupama Gadde <anupamagadde@yahoo.com>
2019-11-18 21:43:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,7 +28,8 @@ function prependKitten(name){
   return newList
 }
 function removeLastKitten() {
-return  kittens.slice(0, kittens.length - 1);
-  //newList.slice(0, newList.length - 1);
-
+  return  kittens.slice(0, kittens.length - 1);
+}
+function removeFirstKitten() {
+  return kittens.slice(1);
 }
